a395b5436dd874e206c40fb5ca1f59bf206afc43
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2022-12-16 23:42:10 -0600

    Finished script.js

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", ",", 
 //Function to randomly generate numbers to pull from indices
 function generatePassword() {
   //Prompt to ask the user the specifics
-  var legnth = prompt("Enter legnth of password. 8-128 characters.");
-  if (legnth < 8 || legnth > 128) {
+  var length = prompt("Enter length of password. 8-128 characters.");
+  if (length < 8 || length > 128) {
     alert("Invalid Length")
   }
   var lowercasePrompt = prompt("Lowercase letters? Y/N");
@@ -82,19 +82,53 @@ function generatePassword() {
   }
   //Ifs to determine what function will run with selected criteria
   if (lowercasepromptResult === "Y" && uppercasepromptResult === "Y" && numberspromptResult === "Y" && symbolspromptResult === "Y") {
-    lowercaseletterFunction(Math.floor(legnth / 4) + (legnth % 4))
-    uppercaseletterFunction(Math.floor(legnth/4))
-    numbersFunction(Math.floor(legnth/4))
-    symbolsFunction(Math.floor(legnth/4))
+    lowercaseletterFunction(Math.floor(length / 4) + (length % 4))
+    uppercaseletterFunction(Math.floor(length / 4))
+    numbersFunction(Math.floor(length / 4))
+    symbolsFunction(Math.floor(length / 4))
   } else if (lowercasepromptResult === "Y" && uppercasepromptResult === "Y" && numberspromptResult === "Y") {
-    lowercaseletterFunction(Math.floor(legnth/3)+(legnth%3))
-    uppercaseletterFunction(Math.floor(legnth/3))
-    numbersFunction(Math.floor(legnth/3))
+    lowercaseletterFunction(Math.floor(length / 3) + (length % 3))
+    uppercaseletterFunction(Math.floor(length / 3))
+    numbersFunction(Math.floor(length / 3))
   } else if (lowercasepromptResult === "Y" && uppercasepromptResult === "Y" && symbolspromptResult === "Y") {
-    lowercaseletterFunction(Math.floor(legnth/3)+(legnth%3))
-    uppercaseletterFunction(Math.floor(legnth/3))
-    symbolsFunction(Math.floor(legnth/3))
-  } else if (lowercasepromptResult === "Y" && numberspromptResult === "Y" && symbolspromptResult)
+    lowercaseletterFunction(Math.floor(length / 3) + (length % 3))
+    uppercaseletterFunction(Math.floor(length / 3))
+    symbolsFunction(Math.floor(length / 3))
+  } else if (lowercasepromptResult === "Y" && numberspromptResult === "Y" && symbolspromptResult === "Y") {
+    lowercaseletterFunction(Math.floor(length / 3) + (length % 3))
+    numbersFunction(Math.floor(length / 3))
+    symbolsFunction(Math.floor(length / 3))
+  } else if (uppercasepromptResult === "Y" && numberspromptResult === "Y" && symbolspromptResult === "Y") {
+    uppercaseletterFunction(Math.floor(length / 3) + (length % 3))
+    numbersFunction(Math.floor(length / 3))
+    symbolsFunction(Math.floor(length / 3))
+  } else if (lowercasepromptResult === "Y" && uppercasepromptResult == "Y") {
+    lowercaseletterFunction(Math.floor(length / 2) + (length % 2))
+    uppercaseletterFunction(Math.floor(length / 2))
+  } else if (lowercasepromptResult === "Y" && symbolspromptResult === "Y") {
+    lowercaseletterFunction(Math.floor(length / 2) + (length % 2))
+    symbolsFunction(Math.floor(length / 2))
+  } else if (lowercasepromptResult === "Y" && numberspromptResult === "Y") {
+    lowercaseletterFunction(Math.floor(length / 2) + (length % 2))
+    numbersFunction(Math.floor(length))
+  } else if (uppercasepromptResult === "Y" && numberspromptResult === "Y") {
+    uppercaseletterFunction(Math.floor(length / 2) + (length % 2))
+    numbersFunction(Math.floor(length / 2))
+  } else if (uppercasepromptResult === "Y" && symbolspromptResult === "Y"){
+    uppercaseletterFunction(Math.floor(length/2)+(length%2))
+    symbolsFunction(Math.floor(length/2))
+  } else if (numberspromptResult === "Y" && symbolspromptResult === "Y") {
+    numbersFunction(Math.floor(length/2)+(length%2))
+    symbolsFunction(Math.floor(length/2))
+  } else if (lowercasepromptResult === "Y") {
+    lowercaseletterFunction(length)
+  } else if (uppercasepromptResult === "Y") {
+    uppercaseletterFunction(length)
+  } else if (symbolspromptResult === "Y") {
+    symbolsFunction(length)
+  } else if (numberspromptResult === "Y") {
+    numbersFunction(length)
+  }
   //Output the password
-  console.log(finalPassword)
+  return finalPassword
 }
